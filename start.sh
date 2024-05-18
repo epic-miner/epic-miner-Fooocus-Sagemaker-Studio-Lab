a7a6fcd6207a136e2b850577abb48c5dc9104089
epic miner <155073729+epic-miner@users.noreply.github.com>
2024-05-18 09:31:05 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,37 +1,57 @@
 #!/bin/bash
-if [ ! -d "Fooocus" ]
-then
+set -e
+
+# Set this variable to true to install to the temporary folder, or to false to have the installation in permanent storage.
+install_in_temp_dir=true
+
+if [ ! -d "Fooocus" ]; then
   git clone https://github.com/lllyasviel/Fooocus.git
-  #git clone --depth 1 --branch V2 https://github.com/lllyasviel/Fooocus.git
-  # Create the config file pointing the checkpoints to checkpoints-real-folder
-fi
-cd Fooocus
-git pull
-if [ ! -L ~/.conda/envs/fooocus ]
-then
-    ln -s /tmp/fooocus ~/.conda/envs/
-fi
-eval "$(conda shell.bash hook)"
-if [ ! -d /tmp/fooocus ]
-then
-    mkdir /tmp/fooocus
-    conda env create -f environment.yaml
-    conda activate fooocus
-    pwd
-    ls
-    pip install -r requirements_versions.txt
-    pip install torch torchvision --force-reinstall --index-url https://download.pytorch.org/whl/cu117
-    pip install pyngrok
-    conda install glib -y
-    rm -rf ~/.cache/pip
 fi
 
-# Because the file manager in Sagemaker Studio Lab ignores the folder called "checkpoints"
-# we need to move checkpoint files into a folder with a different name
+cd Fooocus
+git pull
+
+if [ "$install_in_temp_dir" = true ]; then
+  echo "Installation folder: /tmp/fooocus_env"
+  if [ ! -L ~/.conda/envs/fooocus ]; then
+    echo "Removing existing ~/.conda/envs/fooocus if any"
+    rm -rf ~/.conda/envs/fooocus || true
+    ln -s /tmp/fooocus_env ~/.conda/envs/fooocus
+  fi
+else
+  echo "Installation folder: ~/.conda/envs/fooocus"
+  if [ -L ~/.conda/envs/fooocus ]; then
+    rm ~/.conda/envs/fooocus
+  fi
+fi
+
+eval "$(conda shell.bash hook)"
+
+if [ "$install_in_temp_dir" = true ] && [ ! -d /tmp/fooocus_env ] || [ "$install_in_temp_dir" = false ] && [ ! -d ~/.conda/envs/fooocus ]; then
+  echo "Installing"
+  if [ "$install_in_temp_dir" = true ] && [ ! -d /tmp/fooocus_env ]; then
+    echo "Creating /tmp/fooocus_env"
+    mkdir /tmp/fooocus_env
+  fi
+
+  conda env create -f environment.yaml
+  conda activate fooocus
+
+  pip install -r requirements_versions.txt
+  pip install torch torchvision --force-reinstall --index-url https://download.pytorch.org/whl/cu117
+  pip install opencv-python-headless
+  pip install pyngrok
+
+  rm -f /opt/conda/.condarc
+  conda install -y conda-forge::glib
+  rm -rf ~/.cache/pip
+fi
+
+# Handling configuration for checkpoints
 current_folder=$(pwd)
-model_folder=${current_folder}/models/checkpoints-real-folder
-if [ ! -e config.txt ]
-then
+model_folder="${current_folder}/models/checkpoints-real-folder"
+
+if [ ! -e config.txt ]; then
   json_data="{ \"path_checkpoints\": \"$model_folder\" }"
   echo "$json_data" > config.txt
   echo "JSON file created: config.txt"
@@ -40,19 +60,18 @@ else
   jq --arg new_value "$model_folder" '.path_checkpoints = $new_value' config.txt > config_tmp.txt && mv config_tmp.txt config.txt
 fi
 
-# If the checkpoints folder exists, move it to the new checkpoints-real-folder
-if [ ! -L models/checkpoints ]
-then
-    mv models/checkpoints models/checkpoints-real-folder
-    ln -s models/checkpoints-real-folder models/checkpoints
+# Moving checkpoints folder if needed and updating symlink
+if [ ! -L models/checkpoints ]; then
+  mv models/checkpoints models/checkpoints-real-folder || true
+  ln -s models/checkpoints-real-folder models/checkpoints
 fi
 
 conda activate fooocus
+
 cd ..
-if [ $# -eq 0 ]
-then
-  python start-ngrok.py 
-elif [ $1 = "reset" ]
-then
-  python start-ngrok.py --reset 
+
+if [ $# -eq 0 ]; then
+  python start-ngrok-zrok.py
+elif [ "$1" = "reset" ]; then
+  python start-ngrok-zrok.py --reset
 fi
