00c5aed2a9b8bc8e6fcb34f032b70ae7b1327763
epic miner <155073729+epic-miner@users.noreply.github.com>
2024-05-18 14:51:27 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,67 +1,81 @@
 #!/bin/bash
 
-# Set the installation directory based on the install_in_temp_dir variable
+# Set this variable to true to install to the temporary folder, or to false to have the installation in permanent storage.
 install_in_temp_dir=true
-install_folder="~/.conda/envs/fooocus"
 
-# Check if the Fooocus directory exists, if not, clone the repository
-if [ ! -d "Fooocus" ]; then
+if [ ! -d "Fooocus" ]
+then
   git clone https://github.com/lllyasviel/Fooocus.git
 fi
-
-# Navigate to the Fooocus directory and pull the latest changes
-cd Fooocus || exit
+cd Fooocus
 git pull
-
-# Display the installation directory based on the install_in_temp_dir variable
-echo "Installation directory: $install_folder"
-
-# If install_in_temp_dir is true, create a temporary installation directory and set up a symbolic link
-if [ "$install_in_temp_dir" = true ]; then
-  temporary_install_folder="/tmp/fooocus_env"
-  echo "Temporary installation folder: $temporary_install_folder"
-  if [ ! -L ~/.conda/envs/fooocus ]; then
-    echo "Removing existing symbolic link ~/.conda/envs/fooocus"
+if [ "$install_in_temp_dir" = true ]
+then
+  echo "Installation folder: /tmp/fooocus_env"
+  if [ ! -L ~/.conda/envs/fooocus ]
+  then
+    echo "removing ~/.conda/envs/fooocus"
     rm -rf ~/.conda/envs/fooocus
     rmdir ~/.conda/envs/fooocus
-    ln -s "$temporary_install_folder" ~/.conda/envs/fooocus
+    ln -s /tmp/fooocus_env ~/.conda/envs/fooocus
+  fi
+else
+  echo "Installation folder: ~/.conda/envs/fooocus"
+  if [ -L ~/.conda/envs/fooocus ]
+  then
+    rm ~/.conda/envs/fooocus
   fi
 fi
-
-# Activate the Conda environment
 eval "$(conda shell.bash hook)"
-
-# Verify if the installation folders exist
-if [ ! -d ~/.conda/envs/fooocus ]; then 
-  echo ".conda/envs/fooocus is not a directory or does not exist"
+if [ ! -d ~/.conda/envs/fooocus ]
+then 
+    echo ".conda/envs/fooocus is not a directory or does not exist"
+fi
+if [ ! -d /tmp/fooocus_env ] 
+then
+    echo "/tmp/fooocus_env is currently not a directory"
+fi
+if [ "$install_in_temp_dir" = true ] && [ ! -d /tmp/fooocus_env ] || [ "$install_in_temp_dir" = false ] && [ ! -d ~/.conda/envs/fooocus ]
+then
+    echo "Installing"
+    if [ "$install_in_temp_dir" = true ] && [ ! -d /tmp/fooocus_env ]
+    then
+        echo "Creating /tmp/fooocus_env"
+        mkdir /tmp/fooocus_env
+    fi
+    conda env create -f environment.yaml
+    conda activate fooocus
+    pwd
+    ls
+    pip install -r requirements_versions.txt
+    pip install torch torchvision --force-reinstall --index-url https://download.pytorch.org/whl/cu117
+    pip install opencv-python-headless
+    
+    # Install ngrok
+    pip install pyngrok
+    
+    # Install openssh for pinggy
+    #conda config --add channels conda-forge
+    #conda config --set channel_priority strict
+    #conda install openssh -y
+    
+    # Install zrok
+    #mkdir /home/studio-lab-user/zrok
+    #wget https://github.com/openziti/zrok/releases/download/v0.4.23/zrok_0.4.23_linux_amd64.tar.gz -O /home/studio-lab-user/zrok/zrok.tar.gz
+    #tar -xvf /home/studio-lab-user/zrok/zrok.tar.gz -C /home/studio-lab-user/zrok
+    #chmod a+x /home/studio-lab-user/zrok/zrok 
+    
+    rm -f /opt/conda/.condarc
+    conda install -y conda-forge::glib
+    rm -rf ~/.cache/pip
 fi
 
-# Perform installation if the installation directories are missing
-if [ "$install_in_temp_dir" = true ] && [ ! -d /tmp/fooocus_env ] || [ "$install_in_temp_dir" = false ] && [ ! -d ~/.conda/envs/fooocus ]; then
-  echo "Installing dependencies"
-  
-  # Create Conda environment and install dependencies
-  conda env create -f environment.yaml
-  conda activate fooocus
-  pwd
-  ls
-  pip install -r requirements_versions.txt
-  pip install torch torchvision --force-reinstall --index-url https://download.pytorch.org/whl/cu117
-  pip install opencv-python-headless
-  
-  # Install ngrok
-  pip install pyngrok
-  
-  # Install glib
-  rm -f /opt/conda/.condarc
-  conda install -y conda-forge::glib
-  rm -rf ~/.cache/pip
-fi
-
-# Move checkpoint files into a folder with a different name
+# Because the file manager in Sagemaker Studio Lab ignores the folder called "checkpoints"
+# we need to move checkpoint files into a folder with a different name
 current_folder=$(pwd)
-model_folder="${current_folder}/models/checkpoints-real-folder"
-if [ ! -e config.txt ]; then
+model_folder=${current_folder}/models/checkpoints-real-folder
+if [ ! -e config.txt ]
+then
   json_data="{ \"path_checkpoints\": \"$model_folder\" }"
   echo "$json_data" > config.txt
   echo "JSON file created: config.txt"
@@ -71,18 +85,18 @@ else
 fi
 
 # If the checkpoints folder exists, move it to the new checkpoints-real-folder
-if [ ! -L models/checkpoints ]; then
-  mv models/checkpoints models/checkpoints-real-folder
-  ln -s models/checkpoints-real-folder models/checkpoints
+if [ ! -L models/checkpoints ]
+then
+    mv models/checkpoints models/checkpoints-real-folder
+    ln -s models/checkpoints-real-folder models/checkpoints
 fi
 
-# Activate fooocus environment and start the application
 conda activate fooocus
 cd ..
-
-# Check for command-line arguments
-if [ $# -eq 0 ]; then
+if [ $# -eq 0 ]
+then
   python start-ngrok-zrok.py 
-elif [ "$1" = "reset" ]; then
+elif [ $1 = "reset" ]
+then
   python start-ngrok-zrok.py --reset 
 fi
